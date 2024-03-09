0cf916300cc9f9e5d5510d57cb6ff7f60704589a
Madhu-SS <148480625+Madhu-SS@users.noreply.github.com>
2024-03-09 10:36:37 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+# for instaling local pakages in v env
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenrator',
+    version='0.0.1',
+    author='madhu s s',
+    author_email='madhugowda426@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
